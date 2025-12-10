0ab3e5205f6f1c1f023d1759ba5c517bf8e9f4b1
Fred Fonseca <freddcfonseca@gmail.com>
2025-12-10 02:06:17 -0300

    fix: change logo visuals

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -17,7 +17,7 @@ export const Header = () => {
       <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
         <div className="pt-8 pl-4 pb-7 flex items-center gap-x-1">
           <Image src="/images/cadu/cadu-core.png" alt="Logo" height={40} width={40} />
-          <h1 className="text-2xl font-bold text-indigo-600 tracking-wide">Coduo</h1>
+          <h1 className="text-3xl font-bold text-indigo-600 tracking-wide">coduo</h1>
         </div>
         <ClerkLoading>
           <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
